a4c97b01f5a3ec0aff06a0463a010b28fc155ddb
Hector Airy <hola@hectorairy.com>
2021-12-07 22:39:03 -0600

    Add react suite library

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import "rsuite/dist/rsuite.min.css";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
