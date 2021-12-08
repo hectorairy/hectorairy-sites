98cf4e0b69f00bf097e49fa48a415d6e220b9e28
Hector Airy <hola@hectorairy.com>
2021-12-07 22:10:09 -0600

    PAdded about me section

--- src/App.js
+++ src/App.js
@@ -12,7 +12,21 @@ function App() {
         style={{ marginTop: 50 }}
         round
       />
-      <p>@hectorairy</p>
+      <p className="bold">@hectorairy</p>
+      <h1>
+        Hello world!{"  "}
+        <img
+          src="https://s.w.org/images/core/emoji/13.1.0/svg/1f44b-1f3fc.svg"
+          alt="hello"
+          width={30}
+        />
+        {"  "}my name is Hector Airy.
+      </h1>
+      <p>
+        A Passionate about designing and developing impactful technology. An
+        enthusiastic of long term thinking. Faithful believer that there’s
+        nothing you cannot learn.
+      </p>
       {/* 49263990584 */}
     </div>
   );
