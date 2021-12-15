be1a92b0cc2af3ea79f0af7b02a3bce2b8e92c34
Hector Airy <hola@hectorairy.com>
2021-12-14 18:27:05 -0600

    Improving SEO

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Avatar from "react-avatar";
+import { Helmet } from "react-helmet";
 import {
   FaDragon,
   FaGithub,
@@ -14,6 +15,19 @@ import { Link } from "./components/Link";
 function App() {
   return (
     <div className="container">
+      <Helmet>
+        <meta
+          name="description"
+          content="Find all the best quality products your pet may need"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Amcelium" />
+        <meta name="copyright" content="Amcelium" />
+        <meta
+          name="image"
+          content="https://scontent.fmex12-1.fna.fbcdn.net/v/t39.30808-1/p320x320/259438470_104459778731870_5383511588016407720_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=0c64ff&_nc_ohc=n9Q8ItcTy3cAX84GgYD&_nc_ht=scontent.fmex12-1.fna&edm=AOf6bZoEAAAA&oh=00_AT8hQF-a1DU2F0x7s4AYBeXPNg_Dxv85FkqPvohUdp1I2Q&oe=61BE160B"
+        />
+      </Helmet>
       <Avatar
         facebookId="104457655398749"
         size="150"
--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Helmet } from "react-helmet";
 
 import "./index.css";
 
-ReactDOM.render(
-  <>
-    <Helmet>
-      <meta
-        property="image"
-        content="https://scontent.fmex12-1.fna.fbcdn.net/v/t39.30808-1/p320x320/259438470_104459778731870_5383511588016407720_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=0c64ff&_nc_ohc=n9Q8ItcTy3cAX84GgYD&_nc_ht=scontent.fmex12-1.fna&edm=AOf6bZoEAAAA&oh=00_AT8hQF-a1DU2F0x7s4AYBeXPNg_Dxv85FkqPvohUdp1I2Q&oe=61BE160B"
-      />
-      <meta
-        property="description"
-        content="Card presentation from Hector Airy"
-      />
-    </Helmet>
-    <App />
-  </>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
